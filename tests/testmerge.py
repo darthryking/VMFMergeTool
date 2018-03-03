2fbcefb226eb058f94fa644582faed5667f868e9
Ryan Lam <darth.ryking@gmail.com>
2018-03-03 17:56:12 -0500

    Add test case

--- tests/testmerge.py
+++ tests/testmerge.py
@@ -117,16 +117,74 @@ class TestMerge(unittest.TestCase):
             ]
         )
         
-        try:
+        with self.assertRaises(vmfdelta.DeltaMergeConflict) as contextManager:
             vmfdelta.merge_delta_lists([deltas1, deltas2])
-        except vmfdelta.DeltaMergeConflict as e:
-            actual = get_properties(e.partialDeltas)
-            conflicts = get_properties(e.conflictedDeltas)
-        else:
-            self.fail("Did not throw DeltaMergeConflict!")
             
+        exception = contextManager.exception
+        actual = get_properties(exception.partialDeltas)
+        conflicts = get_properties(exception.conflictedDeltas)
+        
         self.assertEqual(expected, actual)
         self.assertEqual(expectedConflicts, conflicts)
+        
+    def test_merge_conflict_3(self):
+        deltas1 = [
+            vmfdelta.ChangeObject(VMF.SOLID, 1),
+            vmfdelta.AddProperty(VMF.SOLID, 1, 'key', 'value1'),
+        ]
+        
+        deltas2 = [
+            vmfdelta.ChangeObject(VMF.SOLID, 1),
+            vmfdelta.AddProperty(VMF.SOLID, 1, 'key', 'value2'),
+        ]
+        
+        deltas3 = [
+            vmfdelta.RemoveObject(VMF.SOLID, 1),
+        ]
+        
+        expected = get_properties(
+            [
+                vmfdelta.RemoveObject(VMF.SOLID, 1),
+            ]
+        )
+        
+        expectedConflicts = get_properties(
+            [
+                vmfdelta.ChangeObject(VMF.SOLID, 1),
+                vmfdelta.AddProperty(VMF.SOLID, 1, 'key', 'value1'),
+                vmfdelta.AddProperty(VMF.SOLID, 1, 'key', 'value2'),
+            ]
+        )
+        
+        with self.assertRaises(vmfdelta.DeltaMergeConflict) as contextManager:
+            vmfdelta.merge_delta_lists([deltas1, deltas2, deltas3])
+            
+        exception = contextManager.exception
+        actual = get_properties(exception.partialDeltas)
+        conflicts = get_properties(exception.conflictedDeltas)
+        
+        self.assertEqual(expected, actual)
+        self.assertEqual(expectedConflicts, conflicts)
+        
+    def test_merge_outputs_1(self):
+        deltas1 = [
+            vmfdelta.AddOutput(42, 'OnPressed', 'value1', 0),
+        ]
+        deltas2 = [
+            vmfdelta.AddOutput(42, 'OnPressed', 'value2', 0),
+        ]
+        
+        expected = get_properties(
+            [
+                vmfdelta.AddOutput(42, 'OnPressed', 'value1', 0),
+                vmfdelta.AddOutput(42, 'OnPressed', 'value2', 0),
+            ]
+        )
+        actual = get_properties(
+            vmfdelta.merge_delta_lists([deltas1, deltas2])
+        )
+        
+        self.assertEqual(expected, actual)
         
         
 def get_properties(objects):
